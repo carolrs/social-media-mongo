6ac9b4bd48a5cf6fb6200f5b0e73669344585978
Carolina R <anacarolinarodriguesoares@gmail.com>
2021-05-17 13:37:03 +0100

    Method Query

--- src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/PostResource.java
+++ src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/PostResource.java
@@ -5,6 +5,7 @@ import io.carolrs.socialmedia.socialmedia.domain.Post;
 import io.carolrs.socialmedia.socialmedia.domain.User;
 import io.carolrs.socialmedia.socialmedia.domain.resource.service.PostService;
 import io.carolrs.socialmedia.socialmedia.domain.resource.service.UserService;
+import io.carolrs.socialmedia.socialmedia.domain.resource.util.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +25,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "")String text) {
+        text = URL.decodeParam(text);
+        List<Post> list= service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/repository/PostRepository.java
+++ src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/repository/PostRepository.java
@@ -5,7 +5,12 @@ import io.carolrs.socialmedia.socialmedia.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+    //busca query
 
 }
