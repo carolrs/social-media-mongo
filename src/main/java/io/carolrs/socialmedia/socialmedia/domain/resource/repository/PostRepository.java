1c423f204bb619662cdae644664abdee7f1c2168
Carolina R <anacarolinarodriguesoares@gmail.com>
2021-05-17 14:21:30 +0100

    Method Query (Consultation)

--- src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/repository/PostRepository.java
+++ src/main/java/io/carolrs/socialmedia/socialmedia/domain/resource/repository/PostRepository.java
@@ -3,12 +3,16 @@ package io.carolrs.socialmedia.socialmedia.domain.resource.repository;
 import io.carolrs.socialmedia.socialmedia.domain.Post;
 import io.carolrs.socialmedia.socialmedia.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query ("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
     //busca query
